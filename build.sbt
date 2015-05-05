cec7937663ee6634fd6c1f6966f8b1a1cb79f482
Chris Duraj <christopherduraj@gmail.com>
2015-05-05 11:00:16 +0100

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "shopping_cart"
+
+version := "1.0"
+
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "2.1.3" % "test"
---
+++ src/test/scala/test.scala
@@ -0,0 +1,11 @@
+
+import org.scalatest._
+
+
+class ShoppingCartTests extends FlatSpec  with Matchers {
+
+   "Shopping cart" should "be created" in {
+      fail("test fail")
+   }
+
+}
