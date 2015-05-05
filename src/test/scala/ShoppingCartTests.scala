c6002ab1a098f78371c564fcdc553bd891a56f3f
Chris Duraj <christopherduraj@gmail.com>
2015-05-05 13:32:50 +0100

    Names refactoring

--- src/test/scala/ShoppingCartTests.scala
+++ src/test/scala/ShoppingCartTests.scala
@@ -13,41 +13,31 @@ class ShoppingCartTests extends FlatSpec  with Matchers {
 
   it should "sum the total price of 1 apple correctly" in {
     val sutSC = createNewShoppingCart()
-
     sutSC.addProduct("Apple")
-
     sutSC.calculateTotal should be("£0.6")
   }
 
   it should "sum the total price of 1 orange correctly" in {
     val sutSC = createNewShoppingCart()
-
     sutSC.addProduct("Orange")
-
     sutSC.calculateTotal should be("£0.25")
   }
 
   it should "sum the total price of multiple apples correctly" in {
     val sutSC = createNewShoppingCart()
-
     sutSC.addProducts(List("Apple", "Apple", "Apple"))
-
     sutSC.calculateTotal should be("£1.8")
   }
 
   it should "sum the total price of multiple oranges correctly" in {
     val sutSC = createNewShoppingCart()
-
     sutSC.addProducts(List("Orange", "Orange", "Orange"))
-
     sutSC.calculateTotal should be("£0.75")
   }
 
   it should "sum the total price of multiple items correctly" in {
     val sutSC = createNewShoppingCart()
-
     sutSC.addProducts(List("Orange", "Apple", "Apple", "Orange", "Orange"))
-
     sutSC.calculateTotal should be("£1.95")
   }
 
@@ -154,7 +144,6 @@ class ShoppingCartTests extends FlatSpec  with Matchers {
     sutSC.addProduct("Orange")
     sutSC.calculateTotal should be("£2.2")
   }
-
 
   private def createNewShoppingCart(withAppleDeal: Boolean = false, withOrangeDeal:Boolean = false): ShoppingCart =
     new ShoppingCart(withAppleDeal, withOrangeDeal)
